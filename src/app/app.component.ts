96cb9acc87c17918381db707ffdac49ec98ebc16
Carmen Birkle <carmen.birkle@iu-study.org>
2023-06-14 17:37:14 +0200

    adjust icon and hoverclass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import {
   Component,
   ViewChild,
-  AfterViewInit,
   ChangeDetectorRef,
 } from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
@@ -18,8 +17,16 @@ export class AppComponent {
 
   isMobileView = true;
 
+  /**
+   * Constructor of AppComponent
+   * @param {ChangeDetectorRef} cdr - Injected service for managing change detection.
+   */
   constructor(private cdr: ChangeDetectorRef) {}
 
+  /**
+   * Angular's AfterViewInit lifecycle hook, executes after the component's view (and child views) has been initialized.
+   * Here, we're setting up the viewport checker and event listener for window resizing.
+   */
   ngAfterViewInit() {
     this.checkViewport();
     this.cdr.detectChanges();
@@ -30,14 +37,20 @@ export class AppComponent {
     });
   }
 
+  /**
+   * This method checks the viewport width to decide the mode of the drawer.
+   * If the width is less than or equal to 768px, it assumes that the user is in mobile view and sets the drawer mode accordingly.
+   */
   checkViewport() {
     this.isMobileView = window.innerWidth <= 768; // TODO Adjust the breakpoint as needed
-
-    // Set the attributes based on viewport
     this.drawer.opened = !this.isMobileView;
     this.drawer.mode = this.isMobileView ? 'over' : 'side';
   }
 
+  /**
+   * This method is called when a link in the sidenav is clicked.
+   * If the user is in mobile view, it will close the sidenav.
+   */
   onLinkClicked() {
     if (this.isMobileView) {
       this.drawer.close(); // Close the navbar after clicking a link in the mobile view

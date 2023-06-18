5768391bfc073d655508dd916f2437cc8c44aba4
a13xl <general01@tuta.io>
2023-06-18 09:41:20 +0200

    added show/hide password

--- src/app/service/authentication.service.ts
+++ src/app/service/authentication.service.ts
@@ -1,11 +1,13 @@
 import { EventEmitter, Injectable, Output } from '@angular/core';
 import { createUserWithEmailAndPassword, getAuth, signInAnonymously, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
   isLoggedIn = false;
+  errorMsg = null;
   @Output() isLogout = new EventEmitter<void>();
 
   async sigup(email: string, password: string) {
@@ -27,18 +29,19 @@ export class AuthenticationService {
   }
 
   async signin(email: string, password: string) {
+    this.errorMsg = null;
     const auth = getAuth();
-    signInWithEmailAndPassword(auth, email, password)
+    await signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in 
         const user = userCredential.user;
         console.log(user, 'logged in');
         this.isLoggedIn = true;
-        // ...
       })
       .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
+        /* const errorCode = error.code;
+        const errorMessage = error.message; */
+        this.errorMsg = error.message;
       });
   }
 
--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { AuthenticationService } from '../service/authentication.service';
 
 @Component({
@@ -11,20 +11,36 @@ export class SignInComponent {
   // pw: test123
   // username: MusterMax
 
+  @ViewChild('passwordSignIn') pwdField: ElementRef<HTMLInputElement>;
+
+  errorMsg = null;
+  pwdVisible = false;
+  pwdShow = 'assets/img/icons/eye.png';
+  pwdHide = 'assets/img/icons/eye-crossed-out.png';
+  pwdImg: any = this.pwdShow;
+
   constructor(public authentication: AuthenticationService) {
-    if(localStorage.getItem('user')!== null) {
+    /* if(localStorage.getItem('user')!== null) {
       authentication.isLoggedIn = true;
     } else {
       authentication.isLoggedIn = false;
-    }
+    } */
+    this.pwdField = {} as ElementRef<HTMLInputElement>;
   }
 
   async signIn(email: string, password: string) {
     await this.authentication.signin(email, password);
-
+    this.errorMsg = this.authentication.errorMsg;
   }
 
   async signInAnonymously() {
-    await this.authentication.signinAnonymously();
+    //await this.authentication.signinAnonymously();
+    this.signIn('guest@user.com', 'sl4ck-Gu3st')
+  }
+
+  toggleShowPwd() {
+    this.pwdVisible = !this.pwdVisible;
+    this.pwdField.nativeElement.type = this.pwdVisible ? 'text' : 'password';
+    this.pwdImg = this.pwdVisible ? this.pwdHide : this.pwdShow;
   }
 }

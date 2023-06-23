277f4b02add4313f86333eda66eb70c6c2c0789a
Carmen Birkle <carmen.birkle@iu-study.org>
2023-06-23 06:04:02 +0200

    move show date from post to chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -171,5 +171,27 @@ export class ChatComponent implements OnInit, OnDestroy {
       data: { channelId: this.channelId },
     });
   }
+
+  getPreviousPost(currentPost: Post): Post | undefined {
+    const currentIndex = this.allPosts.indexOf(currentPost);
+    if (currentIndex > 0) {
+      return this.allPosts[currentIndex - 1];
+    }
+    return undefined;
+  }
+
+  shouldShowDate(previousPost: Post | undefined, currentPost: Post): boolean {
+    console.log('previousPost:', previousPost);
+    console.log('currentPost:', currentPost);
+
+    if (previousPost && currentPost) {
+      const previousDate = new Date(
+        previousPost.timestamp
+      ).toLocaleDateString();
+      const currentDate = new Date(currentPost.timestamp).toLocaleDateString();
+      return previousDate !== currentDate;
+    }
+    return false;
+  }
 }
 

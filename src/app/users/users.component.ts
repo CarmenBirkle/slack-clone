4b83c3cda39d2eadc2e3773257f3a09f5110a5f2
Lovasz Alexander <alexander.lovasz@annahuette.com>
2023-07-11 13:58:22 +0200

    added search bar

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FirestoreUserService } from '../service/firestore-user.service';
+import { User } from 'src/models/user.class';
 
 @Component({
   selector: 'app-users',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent {
+  users: any;
+
+  constructor(private firestoreUser: FirestoreUserService) {
+    this.getUsers();
+  }
+
+  async getUsers() {
+    this.users = await this.firestoreUser.getUsers();
+    /* this.user.sort((a:any, b: any) => {
+      if(a.username < b.username) {
+        return -1;
+      }
+    }); */
+
+    console.log(this.users);
+    //console.log('Entries:', await Object.keys(this.user).length);
+  }
 
 }

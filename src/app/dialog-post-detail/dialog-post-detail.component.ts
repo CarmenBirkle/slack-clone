a0398df14870db78368f796a0f971bfe58e42a9e
Carmen Birkle <carmen.birkle@iu-study.org>
2023-07-23 11:28:37 +0200

    render replies

--- src/app/dialog-post-detail/dialog-post-detail.component.ts
+++ src/app/dialog-post-detail/dialog-post-detail.component.ts
@@ -11,6 +11,8 @@ import {
   getDocs,
   updateDoc,
 } from '@angular/fire/firestore';
+import { FirestoreUserService } from '../service/firestore-user.service'; 
+
 @Component({
   selector: 'app-dialog-post-detail',
   templateUrl: './dialog-post-detail.component.html',
@@ -23,7 +25,8 @@ export class DialogPostDetailComponent {
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<DialogPostDetailComponent>,
-    private firestore: Firestore
+    private firestore: Firestore,
+    private userService: FirestoreUserService
   ) {
     console.log('postId:', data.postId);
     console.log('postData:', data.postData);
@@ -34,24 +37,58 @@ export class DialogPostDetailComponent {
     this.getReplies();
   }
 
+  // async getReplies() {
+  //   for (let replyId of this.post.replay) {
+  //     console.log('Getting reply with ID:', replyId);
+  //     let docRef = doc(this.firestore, 'replys', replyId);
+  //     let docSnap = await getDoc(docRef);
+
+  //     if (docSnap.exists()) {
+  //       console.log('Found reply:', docSnap.data());
+  //       let replyData = docSnap.data();
+  //       if (replyData && replyData['timestamp']) {
+  //         replyData['timestamp'] = new Date(replyData['timestamp']);
+  //         this.replies.push(replyData);
+  //       }
+  //     } else {
+  //       console.log('No reply found with ID:', replyId);
+  //     }
+  //   }
+  //   console.log('Replies:', this.replies);
+  // }
 
   async getReplies() {
+    this.replies = []; // Zurücksetzen der Array vor dem Laden neuer Daten
     for (let replyId of this.post.replay) {
-      console.log('Getting reply with ID:', replyId); 
+      console.log('Getting reply with ID:', replyId);
       let docRef = doc(this.firestore, 'replys', replyId);
       let docSnap = await getDoc(docRef);
 
       if (docSnap.exists()) {
-        console.log('Found reply:', docSnap.data()); 
-        this.replies.push(docSnap.data());
+        console.log('Found reply:', docSnap.data());
+        let replyData = docSnap.data();
+        if (replyData && replyData['timestamp']) {
+          replyData['timestamp'] = new Date(replyData['timestamp']);
+
+          // Benutzerdetails laden und an Antwortdaten anhängen
+          const userId = replyData['user_id'];
+          const user = await this.userService.getUser(userId);
+          if (user) {
+            replyData['username'] = user.username;
+            replyData['userImage'] = user.photo;
+          } else {
+            replyData['username'] = 'Unbekannt';
+            replyData['userImage'] = ''; // Du könntest hier einen Platzhalter für ein unbekanntes Benutzerbild einfügen
+          }
+
+          this.replies.push(replyData);
+        }
       } else {
-        console.log('No reply found with ID:', replyId); 
+        console.log('No reply found with ID:', replyId);
+      }
     }
     console.log('Replies:', this.replies);
   }
-}
-
- 
 
   closeDialog() {
     this.dialogRef.close();

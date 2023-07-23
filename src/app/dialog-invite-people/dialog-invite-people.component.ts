123a3eaee014c5c0c79da93ac668c673400cc911
a13xl <general01@tuta.io>
2023-07-23 17:13:33 +0200

    updated input validation

--- src/app/dialog-invite-people/dialog-invite-people.component.ts
+++ src/app/dialog-invite-people/dialog-invite-people.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SharedService } from '../service/shared.service';
+import { FormValidationService } from '../service/form-validation.service';
 
 @Component({
   selector: 'app-dialog-invite-people',
@@ -8,13 +9,22 @@ import { SharedService } from '../service/shared.service';
 })
 export class DialogInvitePeopleComponent {
   appComponentContent: any;
+  emailErrorTxt: string = '';
 
-  constructor(public sharedService: SharedService) {
+  constructor(public sharedService: SharedService,
+    private formValidation: FormValidationService) {
     this.appComponentContent = this.sharedService.appComponentContent;
   }
 
-  sendInvite() {
-    console.log();
+  async sendInviteBtn(inputString: string) {
+    const emailOk = await this.valEmail(inputString);
+    //console.log(emailOk);
+  }
+
+  sendInviteKey(event: any, inputString: string) {
+    if (event.key === "Enter") {
+      this.sendInviteBtn(inputString);
+    }
   }
 
   copyInviteLink() {
@@ -22,5 +32,18 @@ export class DialogInvitePeopleComponent {
 
     // Copy the text inside the text field
     navigator.clipboard.writeText(copyText);
+  }
+  
+  async valEmail(email: string) {
+    if(this.formValidation.testEmailFormat(email)) {
+      if(await this.formValidation.testExistEmail(email)) {
+        // Email not exist
+        this.emailErrorTxt = '';
+      } else {
+        this.emailErrorTxt = 'Email already exist';
+      }
+    }
+    // false Email format
+    this.emailErrorTxt = 'Email is invalid';
   }
 }
